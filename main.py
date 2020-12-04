3ace00899f08cd147e5eb41e154cc64d5dd232db
liuyan8 <71243028+liuyan8@users.noreply.github.com>
2020-12-04 16:15:48 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#coding=utf-8  
+__author__ = 'cloudtogo'
+
+from flask import Flask
+import sys
+
+
+app = Flask(__name__)
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+
+@app.route('/')
+def hello():
+    return '恭喜您成功地发布了一个运行在云端的python程序'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080)
