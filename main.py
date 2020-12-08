3f25f520ff5dbe9d4e1350e0e121587f7199a4b0
liuyan8 <71243028+liuyan8@users.noreply.github.com>
2020-12-08 12:27:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ sys.setdefaultencoding('utf8')
 
 @app.route('/')
 def hello():
-    return '恭喜您成功地发布了一个运行在云端的python程序'
+    return '恭喜您成功地发布了一个运行在云端的python程序恭喜您成功地发布了一个运行在云端的python程序'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
